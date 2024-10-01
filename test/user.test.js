f3a8418d12fc9d6da6e7f590cd0a958ace870441
navalnorth <moha_naval@live.com>
2024-10-01 14:56:43 +0200

    test login

--- test/user.test.js
+++ test/user.test.js
@@ -39,3 +39,43 @@ describe('POST /api/users/register', () => {
         expect(response.body.message).toBe('Erreur de connexion à la base de données');
     });
 });
+
+
+
+describe('POST /api/users/login', () => {
+    it('should log in a user', async () => {
+        const user = { id: 1, username: 'test', mdp: await bcrypt.hash('password', 10) };
+        connectToDb.mockResolvedValue({ query: jest.fn().mockResolvedValue([[user]]) });
+        bcrypt.compare.mockResolvedValue(true);
+
+        const response = await request(app)
+            .post('/api/users/login')
+            .send({ username: 'test', mdp: 'password' });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.message).toBe('Utilisateur connecté');
+        expect(response.body.token).toBeDefined();
+    });
+
+    it('should return 401 if user does not exist or password is incorrect', async () => {
+        connectToDb.mockResolvedValue({ query: jest.fn().mockResolvedValue([[]]) });
+
+        const response = await request(app)
+            .post('/api/users/login')
+            .send({ username: 'test', mdp: 'wrongpassword' });
+
+        expect(response.statusCode).toBe(401);
+        expect(response.body.message).toBe('Email ou mot de passe incorrect');
+    });
+
+    it('should return 500 on DB error', async () => {
+        connectToDb.mockResolvedValue(null); // Simule une connexion échouée à la DB
+    
+        const response = await request(app)
+            .post('/api/users/login')
+            .send({ username: 'test', mdp: 'password' });
+    
+        expect(response.statusCode).toBe(500);
+        expect(response.body.message).toBe('Erreur de connexion à la base de données'); // Mettez à jour ce message
+    });
+});
