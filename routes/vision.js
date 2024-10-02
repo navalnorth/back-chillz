edaa5772cf175f574592b00bff5350f8a41fcccc
navalnorth <moha_naval@live.com>
2024-10-02 08:48:33 +0200

    favoris

--- routes/vision.js
+++ routes/vision.js
@@ -1,21 +1,33 @@
-const express = require('express')
-const router = express.Router()
-const bcrypt = require('bcrypt')
-const mysql = require('mysql2')
-console.log("changer d'écran")
-require('dotenv').config();
-const connectToDb = require('../db.js');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const mysql = require("mysql2");
+require("dotenv").config();
+const connectToDb = require("../db.js");
+const jwt = require("jsonwebtoken");
 
 
-router.get('/favoris/:id' , async (req, res) => {
-    const db = await connectToDb()
-    console.log("changer d'écran")
-    if (!db) {return res.status(500).json({ message: 'Erreur de connexion à la base de données' })}
+
+router.post("/favoris/:id", async (req, res) => {
+  try {
+    const db = await connectToDb();
+    if (!db) { return res.status(500).json({ message: "Erreur de connexion à la base de données" })}
 
     const userId = req.params.id
+    const { id_filmAPI } = req.body
+
+    const [dejaFavori] = await db.query( 'SELECT * FROM favori WHERE id_user = ? AND id_filmAPI = ?', [userId, id_filmAPI] )
+    if (dejaFavori.length > 0) { return res.status(400).json({ message: "Le film est dèja dans vos favoris."}) }
+
+    await db.query( "INSERT INTO favori (id_user, id_filmAPI) VALUES (?, ?)", [userId, id_filmAPI])
+
+    return res.status(201).json({ message: "Film ajouté aux favoris avec succès !"})
+  } catch (err) {
+    console.error("Erreur lors de l'ajout du film aux favoris :", err);
+    return res.status(500).json({ message: "Erreur interne du serveur." });
+  }
+});
 
 
-    const [results] = await db.query("")
 
-})
+module.exports = router
